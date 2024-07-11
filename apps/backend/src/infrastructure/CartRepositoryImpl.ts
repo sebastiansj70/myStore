f730be69616d8ba7bc6ea09a8280a462cd9e2008
sebastiansj70 <sebastiansj70@gmail.com>
2024-07-11 16:21:11 -0500

    refactor logic of adding products to cart

--- apps/backend/src/infrastructure/CartRepositoryImpl.ts
+++ apps/backend/src/infrastructure/CartRepositoryImpl.ts
@@ -18,38 +18,90 @@ class CartRepositoryImpl implements CartRepository {
             const products: Product[] = JSON.parse(productsData);
 
             const product = products.find(p => p.id === item.id);
-            const { total = 0 } = cart.price
 
             if (!product) {
                 throw new Error('Producto no encontrado');
             }
 
-            const totalPrice = total + (product.price * item.quantity);
-            const itemTotalPrice = product.price * item.quantity;
-            cart.price.total = totalPrice
+            this.updateCartItems(cart, product, item.quantity);
 
-            const existingItemIndex = cart.items.findIndex((cartItem) => cartItem.id === item.id);
+            const updatedCart = await this.calculate(cart);
+            await fs.writeFile(cartFilePath, JSON.stringify(updatedCart, null, 2));
+
+            return updatedCart
+        } catch (error) {
+            throw new Error('Error al agregar el producto al carro');
+        }
+    }
+
+
+
+    private updateCartItems(cart: Cart, product: Product, quantity: number) {
+        const { total = 0 } = cart.price;
+        const itemTotalPrice = product.price * quantity;
+        const totalPrice = total + itemTotalPrice;
+
+        if (quantity === 0) {
+            // Eliminar el producto del carrito si la cantidad es cero
+            cart.items = cart.items.filter(cartItem => cartItem.id !== product.id);
+        } else {
+            const existingItemIndex = cart.items.findIndex(cartItem => cartItem.id === product.id);
+
             if (existingItemIndex !== -1) {
-                cart.items[existingItemIndex].quantity += item.quantity;
-                cart.items[existingItemIndex].price = {
-                    subtotal: product.price,
-                    total: product.price * cart.items[existingItemIndex].quantity
+                // Actualizar la cantidad y el precio total del ítem existente
+                cart.items[existingItemIndex].quantity += quantity;
+                if (cart.items[existingItemIndex].quantity > 0) {
+                    cart.items[existingItemIndex].price = {
+                        subtotal: product.price,
+                        total: product.price * cart.items[existingItemIndex].quantity
+                    };
+                } else {
+                    //si la cantidad es menos que 1, significa que elimino el prodcuto del carrito
+                    cart.items = cart.items.filter(cartItem => cartItem.id !== product.id);
                 }
             } else {
+                // Añadir un nuevo ítem al carrito
                 cart.items.push({
-                    ...item,
                     ...product,
+                    ...{ id: product.id },
+                    quantity,
                     price: {
                         subtotal: product.price,
                         total: itemTotalPrice
                     }
                 });
             }
+        }
+
+        // Actualizar el precio total del carrito
+        cart.price.total = totalPrice;
+    }
+
+
+    async calculate(cart: Cart): Promise<Cart> {
+        try {
+            const productsData = await fs.readFile(productsFilePath, 'utf8');
+            const products: Product[] = JSON.parse(productsData);
+
+            cart.items.forEach(item => {
+                const product = products.find(p => p.id === item.id);
+                if (product) {
+                    item.price = {
+                        subtotal: product.price,
+                        total: product.price * item.quantity
+                    };
+                }
+            });
+
+            cart.price.total = cart.items.reduce((acc, item) => {
+                const itemTotal = item.price ? item.price.total : 0;
+                return acc + itemTotal;
+            }, 0);
 
             await fs.writeFile(cartFilePath, JSON.stringify(cart, null, 2));
-            return cart
+            return cart;
         } catch (error) {
-            throw new Error('Error al agregar el producto al carro');
+            throw new Error('Error al calcular los precios del carrito');
         }
     }
 }
